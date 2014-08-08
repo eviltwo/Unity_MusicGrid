492a2874663f1e0298214ffa2af804d5660e6310
eviltwo <e135764@DaikiMacBookPro.local>
2014-08-08 09:55:55 +0900

    見えない壁を作成

--- MusicGrid/Assets/Scripts/PlayerController.cs
+++ MusicGrid/Assets/Scripts/PlayerController.cs
@@ -8,9 +8,9 @@ public class PlayerController : MonoBehaviour {
 	public GameObject DamageEffectPrefab;
 
 	Vector2 Speed = Vector2.zero;
-
+	FieldStatus fStatus;
 	void Start () {
-	
+		fStatus = GameObject.Find ("System").gameObject.GetComponent<FieldStatus> ();
 	}
 	
 	// Update is called once per frame
@@ -33,12 +33,19 @@ public class PlayerController : MonoBehaviour {
 		Vector3 AddSpeed = Vector3.zero;
 		AddSpeed.x += Speed.x * Time.deltaTime;
 		AddSpeed.y += Speed.y * Time.deltaTime;
-		transform.position += AddSpeed;
+		transform.localPosition += AddSpeed;
+
+		Vector3 pos = transform.localPosition;
+		for (int i = 0; i < 2; i++) {
+			pos[i] = Mathf.Max (0, pos[i]);
+			pos[i] = Mathf.Min (fStatus.FieldScale[i], pos[i]);
+		}
+		transform.localPosition = pos;
 	}
 
 	void OnTriggerEnter(Collider c){
 		GameObject effect = (GameObject)Instantiate(DamageEffectPrefab);
 		effect.transform.localPosition = transform.localPosition;
-		transform.position = new Vector3 (3.5f,3.5f,transform.position.z);
+		transform.position = new Vector3 (3.5f,3.5f,transform.localPosition.z);
 	}
 }
